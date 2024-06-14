e11d6da5c746139c07c03f7e76877768caf9348b
Moritz Reiter <mail@moritzreiter.de>
2024-06-14 09:19:10 +0200

    implement basic order service features

---
+++ src/main/java/com/polarbookshop/orderservice/config/ClientConfig.java
@@ -0,0 +1,21 @@
+package com.polarbookshop.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.net.http.WebSocket;
+
+@Configuration
+public class ClientConfig {
+
+    @Bean
+    WebClient webClient(
+            ClientProperties clientProperties,
+            WebClient.Builder webClientBuilder
+    ) {
+        return webClientBuilder
+                .baseUrl(clientProperties.catalogServiceUri().toString())
+                .build();
+    }
+}
