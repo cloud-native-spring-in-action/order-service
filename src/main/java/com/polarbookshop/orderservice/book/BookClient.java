2e2cf36b998c4cd96979123c905eec3a71a7e7bf
Moritz Reiter <mail@moritzreiter.de>
2024-06-17 13:53:09 +0200

    add test

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -2,7 +2,9 @@ package com.polarbookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -22,6 +24,11 @@ public class BookClient {
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
                 .bodyToMono(Book.class)
-                .timeout(Duration.ofSeconds(3), Mono.empty());
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        e -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class,
+                        e -> Mono.empty());
     }
 }
